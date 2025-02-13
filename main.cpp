4a18d3fdc7aabf1266464775b14452591e1ae981
Egor Malykh <hello@meownoid.pro>
2025-02-14 00:47:21 +0100

    refactor

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 #define SAMPLE_RATE 96000
 #define SAMPLES_PER_BUFFER 512
 #define BUFFER_COUNT 4
-#define LOWPASS_CUTOFF 4000
+#define LOWPASS_CUTOFF 6000
 
 #define PICO_AUDIO_PACK_I2S_DATA 9
 #define PICO_AUDIO_PACK_I2S_BCLK 10
@@ -13,44 +13,52 @@
 #define Q31_ONE (1 << 31)
 
 // Function to collect entropy from ADC noise
-uint8_t get_adc_random_bit(void)
+uint8_t adc_random_bit()
 {
     adc_select_input(0);
     uint16_t raw_adc_value = adc_read();
-    // Return only the least significant bit
     return raw_adc_value & 1;
 }
 
-// Function to generate an 8-bit random number
-uint8_t get_random_byte(void)
+uint64_t adc_random_number()
 {
-    uint8_t random_byte = 0;
-    for (int i = 0; i < 8; i++)
+    uint64_t result = 0;
+    for (int i = 0; i < 64; i++)
     {
-        random_byte = (random_byte << 1) | get_adc_random_bit();
+        result = (result << 1) | adc_random_bit();
     }
-    return random_byte;
+    return result;
 }
 
-// Function to generate a 64-bit random number
-uint64_t get_random_number(void)
-{
-    uint64_t random_number = 0;
-    for (int i = 0; i < 8; i++)
-    {
-        random_number = (random_number << 8) | get_random_byte();
-    }
-    return random_number;
-}
-
-// *Really* minimal PCG32 code / (c) 2014 M.E. O'Neill / pcg-random.org
-// Licensed under Apache License 2.0 (NO WARRANTY, etc. see website)
+/*
+ * PCG Random Number Generation for C.
+ *
+ * Copyright 2014 Melissa O'Neill <oneill@pcg-random.org>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * For additional information about the PCG random number generation scheme,
+ * including its license and other licensing options, visit
+ *
+ *       http://www.pcg-random.org
+ */
 
 typedef struct
 {
     uint64_t state;
     uint64_t inc;
 } pcg32_random_t;
+
 static pcg32_random_t pcg32_global = {0x853c49e6748fea9bULL, 0xda3e39cb94b95bdbULL};
 
 inline uint32_t pcg32_random_r(pcg32_random_t *rng)
@@ -83,7 +91,7 @@ inline uint32_t pcg32_random()
     return pcg32_random_r(&pcg32_global);
 }
 
-inline int32_t pcg32_normal()
+inline int32_t random_normal()
 {
     uint32_t result = 0;
     for (uint i = 0; i < 6; i++)
@@ -104,7 +112,7 @@ int main()
     stdio_init_all();
     adc_init();
     adc_gpio_init(26); // ADC0
-    pcg32_srandom(get_random_number() ^ time_us_64(), 0);
+    pcg32_srandom(adc_random_number() ^ time_us_64(), 0);
 
     audio_format_t audio_format = {
         .sample_freq = SAMPLE_RATE,
@@ -114,12 +122,14 @@ int main()
 
     struct audio_buffer_format producer_format = {
         .format = &audio_format,
-        .sample_stride = 2};
+        .sample_stride = 2
+    };
 
     struct audio_buffer_pool *producer_pool = audio_new_producer_pool(
         &producer_format,
         BUFFER_COUNT,
-        SAMPLES_PER_BUFFER);
+        SAMPLES_PER_BUFFER
+    );
 
     struct audio_i2s_config config = {
         .data_pin = PICO_AUDIO_PACK_I2S_DATA,
@@ -142,13 +152,13 @@ int main()
 
     audio_i2s_set_enabled(true);
 
-    const int64_t RC_q31_lp = (1LL << 31) / (2 * 314 * LOWPASS_CUTOFF);
-    const int64_t dt_q31_lp = (1LL << 31) / SAMPLE_RATE;
-    const int32_t alpha_lp = (dt_q31_lp * Q31_ONE) / (RC_q31_lp + dt_q31_lp);
-    const int32_t alpha_lp_inv = Q31_ONE - alpha_lp;
+    const int64_t RC_q31 = (1LL << 31) / (2 * 314 * LOWPASS_CUTOFF);
+    const int64_t dt_q31 = (1LL << 31) / SAMPLE_RATE;
+    const int64_t alpha = (dt_q31 * Q31_ONE) / (RC_q31 + dt_q31);
+    const int64_t alpha_inv = Q31_ONE - alpha;
 
     int32_t x = 0;
-    int32_t y_lp = 0;
+    int32_t y = 0;
 
     while (true)
     {
@@ -157,11 +167,11 @@ int main()
         for (uint i = 0; i < buffer->max_sample_count; i++)
         {
             // Integrate normal white noise
-            x = clip16(x + (pcg32_normal() >> 7));
+            x = clip16(x + (random_normal() >> 7));
             // Apply low-pass filter
-            y_lp = (((int64_t)alpha_lp * (int64_t)x) >> 31) + (((int64_t)alpha_lp_inv * (int64_t)y_lp) >> 31);
+            y = ((alpha * (int64_t)x) + (alpha_inv * (int64_t)y)) >> 31;
             // Output to audio buffer
-            samples[i] = clip16(y_lp);
+            samples[i] = clip16(y);
         }
         buffer->sample_count = buffer->max_sample_count;
         give_audio_buffer(producer_pool, buffer);
